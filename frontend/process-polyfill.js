b1072a99597df1d059a1c1c0b171c837e58b43f5
sankalp nadiger <nadigersankalp@gmail.com>
2025-04-25 09:22:39 +0530

    frontend: process config

---
+++ frontend/process-polyfill.js
@@ -0,0 +1,2 @@
+// process-polyfill.js
+window.process = window.process || { env: {} };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  nodePolyfills({
+    // Add all node built-ins including 'process'
+    include: ['process']
+  })
+],
+define: {
+  global: 'globalThis',
+}
 })
